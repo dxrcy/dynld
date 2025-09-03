f0dcf54518549798156a15cc7df7fb654b3e1b04
darcy <acednes@gmail.com>
2025-09-03 11:29:02 +1000

    init

---
+++ example/src/main.zig
@@ -0,0 +1,5 @@
+export const BAR: f64 = 4.1;
+
+export fn foo(x: f64) f64 {
+    return x + 6.7;
+}
---
+++ src/main.zig
@@ -0,0 +1,25 @@
+const std = @import("std");
+
+const DynamicHandler = @import("dynamic.zig").DynamicHandler;
+
+pub fn main() !void {
+    var args = std.process.args();
+    _ = args.next();
+
+    const path = args.next() orelse {
+        std.debug.print("missing argument\n", .{});
+        return error.MissingArgument;
+    };
+
+    const handler = try DynamicHandler(Example).open(path);
+    defer handler.close() catch unreachable;
+    const lib = handler.content;
+
+    std.debug.print("{d:.4}\n", .{lib.BAR});
+    std.debug.print("{d:.4}\n", .{lib.foo(10.0)});
+}
+
+const Example = struct {
+    BAR: f64,
+    foo: *const fn (a: f64) f64,
+};
